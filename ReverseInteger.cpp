ae9d92311ef53289e4206bc6a9b93e7ce0919ef5
Dmitry Morozov <9984398@gmail.com>
2023-01-15 19:27:42 +0300

    ReverseInt : Done Tests : Passing

---
+++ ReverseInteger.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Dmitry Morozov on 15/1/23.
+//
+
+#include <iostream>
+
+class Solution {
+public:
+
+    int reverse(int x) {
+
+        long result_number = 0;
+
+        while (x) {
+            result_number = result_number * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (result_number < INT_MIN || result_number > INT_MAX) {
+            return 0;
+        }
+
+        return static_cast<int>(result_number);
+    }
+
+
+};
+
+int main() {
+    Solution s;
+    std::cout << s.reverse ( 123 ) << std::endl;
+}
